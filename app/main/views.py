5d91eab635bf5820a415ec12af9a1f82bcf5ad0e
KK <karanjakelvin77@gmail.com>
2022-02-28 20:45:31 +0300

    It will add views and routes in the view.py file

--- app/main/views.py
+++ app/main/views.py
@@ -2,4 +2,10 @@ from flask import render_template, request, redirect, url_for
 from . import main
 # from .. requests import 
 from .forms import ReviewForm
-# from ..models import Review
+from ..sources_model import Display
+from ..articles_model import Display
+
+@main.route('/')
+def index():
+
+    return render_template('index.html')
