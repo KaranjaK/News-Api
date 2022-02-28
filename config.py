7fe14b86a4ee0e9e395932e29a0f698f41577181
KK <karanjakelvin77@gmail.com>
2022-02-28 05:34:31 +0300

    It will add the production and development classes and provide a config options library for class access

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from distutils.command.config import config
+from distutils.debug import DEBUG
 import os
 
 class Config:
@@ -5,3 +7,14 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apikey={}'
     NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production': ProdConfig
+}
