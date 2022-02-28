de6ad3a96a6f44e52b0bf398fcbd5df617355a09
KK <karanjakelvin77@gmail.com>
2022-02-28 12:27:01 +0300

    It will define the function to access data on articles

---
+++ app/articles_model.py
@@ -0,0 +1,9 @@
+class Articles:
+
+    def __ini__(self, id, name, title, author, urlToImage, description):
+        self.id = id
+        self.name = name
+        self.title = title
+        self.author = author
+        self.urlToImage = urlToImage
+        self.description = description
