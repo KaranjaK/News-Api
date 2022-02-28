f2cc35739b9dbbef77b03a5e0b2819b552a53068
KK <karanjakelvin77@gmail.com>
2022-02-28 05:29:32 +0300

    It will add the Config class

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 import os
+
+class Config:
+
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apikey={}'
+    NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
