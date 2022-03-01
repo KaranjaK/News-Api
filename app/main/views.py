efb764a518e6f8ac2361e5bc45af75d20482dd8d
KK <karanjakelvin77@gmail.com>
2022-03-01 10:32:28 +0300

    It will define the routes for the app

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
+from ast import arguments
+from turtle import title
 from flask import render_template, request, redirect, url_for
 
-from app.requests import get_source
+from app.requests import get_by_category, get_source, getarticle_source
 from . import main
 # from .. requests import 
 # from .forms import ReviewForm
@@ -13,3 +15,17 @@ def index():
     message = "We made it Mama"
     sources_category = get_source()
     return render_template('index.html', message = message, id = sources_category)
+
+@main.route('/article/<id>')
+def article(id):
+
+    article = getarticle_source(id)
+    return render_template('article.html', id = id, article = article)
+
+@main.route('/categories/<cat_name>')
+def category(cate_name):
+
+    category = get_by_category(cate_name)
+    title = f'{cate_name}'
+    cate = cate_name
+    return render_template('category.html', title=title, category=category, cate=cate)
