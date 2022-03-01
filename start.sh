3dc04bbdf36b3d1042bed2013b41bf61a7d2c645
KK <karanjakelvin77@gmail.com>
2022-03-01 16:25:04 +0300

    It will redeine the ApiKey for authentification

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY = 0351b8e3bbbe4418b1bd78216e6e5841
-export SECRET_KEY = 5050
+export NEWS_API_KEY=0351b8e3bbbe4418b1bd78216e6e5841
+export SECRET_KEY=5050
 
 python3.8 manage.py server
