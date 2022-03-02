7f3562f98cbd2e443b311e64cedd89879789859a
KK <karanjakelvin77@gmail.com>
2022-03-02 14:01:50 +0300

    It will a ulr property

--- app/articles_model.py
+++ app/articles_model.py
@@ -1,11 +1,12 @@
 class Articles:
 
-    def __init__(self, id, name, title, urlToImage, author, publishedAt, description):
+    def __init__(self, id, name, title, urlToImage, author, url, publishedAt, description):
         self.id = id
         self.name = name
         self.title = title
         self.urlToImage = urlToImage
         self.author = author
+        self.url = url
         self.publishedAt = publishedAt
         self.description = description
 
@@ -13,12 +14,13 @@ class Display:
 
     articles_display = []
 
-    def __init__(self, id, name, title, urlToImage, author, publishedAt, description):
+    def __init__(self, id, name, title, urlToImage, author, url, publishedAt, description):
         self.id = id
         self.name = name
         self.title = title
         self.urlToImage = urlToImage
         self.author = author
+        self.url = url
         self.publishedAt = publishedAt
         self.description = description
 
