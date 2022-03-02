b90cf5fa26a0b3c97f78cb8c4b8e4d6c75edf6ee
KK <karanjakelvin77@gmail.com>
2022-03-02 04:47:31 +0300

    It will update the base url

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     CATE_API_URL = 'https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
